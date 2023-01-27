3acb65f66f25db2c75b8885390bea2f94bce47e7
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-01-27 09:04:14 -0800

    add a pizza order prototype that updates price of pizza based on branching logic

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Test: it should create a constructor for pizza info that stores inside
 Code: const pizza = PizzaData(["cheese", "pepperoni"], "large") 
 Expected Output: PizzaData { toppings: ["cheese", "pepperoni"], size: "large" }
 
+Describe: PizzaData.prototype.Order = function()
+
+Test: it should create a prototype of the order logic for price of topping and price of sizing
+Code: const pizzaCost = PizzaData.prototype.Order
+Expeceted Output: 10
+
+Test: it output different cost based on change of topping selection
+Code: let pizza = PizzaData("Cheese", "X-Large")
+Expeceted Output: 13
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
+// Business Logic
+
 function PizzaData(topping, size) {
   this.topping = topping;
   this.size = size;
+  this.pizzaCost = 10
 }
+
+PizzaData.prototype.Order = function() {
+    if ((this.topping === "Cheese") || (this.topping === "Pepperoni")) {
+      this.pizzaCost += 1;
+    }
+    else if ((this.topping === "Chicken") || (this.topping === "Bacon")) {
+      this.pizzaCost += 3;
+    }
+    if (this.size === "X-Small") {
+      this.pizzaCost -= 2;
+    }
+    else if (this.size === "X-Large") {
+      this.pizzaCost += 2;
+    }
+    console.log(this.pizzaCost);
+    return pizzaCost;
+}
+
+// UI Logic
+
